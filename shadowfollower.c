ecc13397c63bbf7cae645e4f5d2550b0cf2b9f2b
Patrick Simmons <pasimmons@amd.com>
2026-02-23 17:45:31 -0600

    Hacks necessary for building on r* machines

---
+++ shadowfollower.c
@@ -0,0 +1,21 @@
+#include <sys/stat.h>
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <string.h>
+
+int stat(const char *restrict path, struct stat *restrict statbuf)
+{
+     int (*real_stat)(const char *restrict path, struct stat *restrict statbuf) = dlsym(((void *) -1l),"stat");
+     if(!strncmp(path,"/opt/rocm",strlen("/opt/rocm")))
+       return -1;
+     return real_stat(path,statbuf);
+}
+
+int lstat(const char *restrict path, struct stat *restrict statbuf)
+{
+     int (*real_lstat)(const char *restrict path, struct stat *restrict statbuf) = dlsym(((void *) -1l),"lstat");
+     if(!strncmp(path,"/opt/rocm",strlen("/opt/rocm")))
+       return -1;
+     return real_lstat(path,statbuf);
+}
---
+++ stat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+	for(int i=1; i<argc; i++)
+		if(!strncmp(argv[i],"/opt/rocm",strlen("/opt/rocm")))
+		{
+			fprintf(stderr,"stat: cannot statx '%s': No such file or directory\n",argv[i]);
+			return 1;
+		}
+
+	execv("/usr/bin/stat",argv);
+}
